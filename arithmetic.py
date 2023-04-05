55f1644144db9e6e2e78e51836faf3c0db5719ac
Jason Park <platosw83@gmail.com>
2023-04-05 12:53:29 -0400

    Add further study func

--- arithmetic.py
+++ arithmetic.py
@@ -48,3 +48,15 @@ def mod(num1, num2):
     """Return the remainder of num1 / num2."""
 
     return num1 % num2
+
+
+def add_mult(num1, num2, num3):
+    """Return the sum of num1 and num2, then multiply sum with num3."""
+
+    return (num1 + num2) * num3
+
+
+def add_cubes(num1, num2):
+    """Add the cubes of num1 and num2."""
+
+    return pow(num1, 3) + pow(num2, 3)
